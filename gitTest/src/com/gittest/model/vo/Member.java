a5c406b92f2a28d0551f747586032ac5b0f76f0c
USER <USER@DESKTOP-4FA3NH9>
2022-06-13 11:07:15 +0900

    Member클래스 setter/getter/생성자 추가

--- gitTest/src/com/gittest/model/vo/Member.java
+++ gitTest/src/com/gittest/model/vo/Member.java
@@ -3,4 +3,25 @@ package com.gittest.model.vo;
 public class Member {
 	private String name;
 	private int age;
+	
+	public Member() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
 }
